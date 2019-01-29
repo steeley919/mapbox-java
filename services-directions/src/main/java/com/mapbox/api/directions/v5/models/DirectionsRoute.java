17b3b63778a5a146b3db78d4001bab133c665553
osana <osana.babayan@mapbox.com>
2019-01-29 16:38:48 -0500

    removing dependency on  PointDeserializer, PointSerializer, BoundingBoxDeserializer and BoundingBoxSerializer, GeometryDeserializer in favor of TypeAdapters

--- services-directions/src/main/java/com/mapbox/api/directions/v5/models/DirectionsRoute.java
+++ services-directions/src/main/java/com/mapbox/api/directions/v5/models/DirectionsRoute.java
@@ -10,7 +10,7 @@ import com.google.gson.annotations.SerializedName;
 import com.mapbox.api.directions.v5.DirectionsAdapterFactory;
 import com.mapbox.api.directions.v5.MapboxDirections;
 import com.mapbox.geojson.Point;
-import com.mapbox.geojson.gson.PointDeserializer;
+import com.mapbox.geojson.PointAsCoordinatesTypeAdapter;
 
 import java.util.List;
 
@@ -146,7 +146,7 @@ public abstract class DirectionsRoute extends DirectionsJsonObject {
   public static DirectionsRoute fromJson(String json) {
     GsonBuilder gson = new GsonBuilder();
     gson.registerTypeAdapterFactory(DirectionsAdapterFactory.create());
-    gson.registerTypeAdapter(Point.class, new PointDeserializer());
+    gson.registerTypeAdapter(Point.class, new PointAsCoordinatesTypeAdapter());
     return gson.create().fromJson(json, DirectionsRoute.class);
   }
 
